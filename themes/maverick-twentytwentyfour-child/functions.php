05d342929436c7d526cdd68a86cfd4bf4f63fbf7
smdimalibot <sm.dimalibot@gmail.com>
2024-10-23 16:56:47 -0400

    Added Menus (in Appearance, WordPress)

--- themes/maverick-twentytwentyfour-child/functions.php
+++ themes/maverick-twentytwentyfour-child/functions.php
@@ -3,3 +3,10 @@ function maverick_twentytwentyfour_enqueue_styles() {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 }
 add_action('wp_enqueue_scripts', 'maverick_twentytwentyfour_enqueue_styles');
+
+function register_my_menus() {
+    register_nav_menus(array(
+        'header-menu' => __('Header Menu'),
+    ));
+}
+add_action('init', 'register_my_menus');
