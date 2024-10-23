2465c2bfef3419ed2977f93b27744973a3f8163d
smdimalibot <sm.dimalibot@gmail.com>
2024-10-23 17:07:18 -0400

    Minor Change to Apperance -> Menus in functions.php

--- themes/maverick-twentytwentyfour-child/functions.php
+++ themes/maverick-twentytwentyfour-child/functions.php
@@ -4,9 +4,11 @@ function maverick_twentytwentyfour_enqueue_styles() {
 }
 add_action('wp_enqueue_scripts', 'maverick_twentytwentyfour_enqueue_styles');
 
-function register_my_menus() {
-    register_nav_menus(array(
-        'header-menu' => __('Header Menu'),
-    ));
+function my_theme_setup() {
+    register_nav_menus( array(
+        'primary'   => __( 'Primary Menu', 'textdomain' ),
+        'footer'    => __( 'Footer Menu', 'textdomain' ),
+    ) );
 }
-add_action('init', 'register_my_menus');
+add_action( 'after_setup_theme', 'my_theme_setup' );
+?>
