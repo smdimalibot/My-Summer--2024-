1491f13828708455d3364d3d004bdd3c551b9810
smdimalibot <sm.dimalibot@gmail.com>
2024-10-23 17:23:53 -0400

    Added Customize for Appearance in functions.php

--- themes/maverick-twentytwentyfour-child/functions.php
+++ themes/maverick-twentytwentyfour-child/functions.php
@@ -11,4 +11,33 @@ function my_theme_setup() {
     ) );
 }
 add_action( 'after_setup_theme', 'my_theme_setup' );
-?>
+
+function my_customizer_settings( $wp_customize ) {
+    $wp_customize->add_section( 'my_custom_settings', array(
+        'title'    => __( 'Custom Settings', 'textdomain' ),
+        'priority' => 30,
+    ));
+
+    $wp_customize->add_setting( 'my_custom_color', array(
+        'default'   => '#ffffff',
+        'transport' => 'refresh',
+    ));
+
+    $wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'my_custom_color_control', array(
+        'label'    => __( 'Custom Color', 'textdomain' ),
+        'section'  => 'my_custom_settings',
+        'settings' => 'my_custom_color',
+    )));
+}
+add_action( 'customize_register', 'my_customizer_settings' );
+
+function my_customizer_css() {
+    ?>
+    <style type="text/css">
+        body {
+            background-color: <?php echo get_theme_mod( 'my_custom_color', '#ff0000' ); ?>;
+        }
+    </style>
+    <?php
+}
+add_action( 'wp_head', 'my_customizer_css' );
