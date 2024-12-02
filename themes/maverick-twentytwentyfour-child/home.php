200364cc23fbcb1973785c1fd11815c32530712d
smdimalibot <sm.dimalibot@gmail.com>
2024-12-02 09:22:35 -0500

    Updated home.php, single.php and style.css in Child Theme

--- themes/maverick-twentytwentyfour-child/home.php
+++ themes/maverick-twentytwentyfour-child/home.php
@@ -1,15 +1,16 @@
-<?php get_header(); ?>
-
-<div class="blog-home">
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <div class="blog-post">
-            <a href="<?php the_permalink(); ?>">
-                <?php the_post_thumbnail('medium'); ?>
-                <h2><?php the_title(); ?></h2>
-            </a>
-            <p><?php the_excerpt(); ?></p>
-        </div>
-    <?php endwhile; endif; ?>
+<?php
+get_header(); ?>
+<div class="custom-blog-home">
+    <h1>Welcome to Our Blog</h1>
+    <?php if (have_posts()) :
+        while (have_posts()) : the_post(); ?>
+            <article>
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <p><?php the_excerpt(); ?></p>
+            </article>
+        <?php endwhile;
+    else : ?>
+        <p>No posts found.</p>
+    <?php endif; ?>
 </div>
-
 <?php get_footer(); ?>
--- themes/maverick-twentytwentyfour-child/single.php
+++ themes/maverick-twentytwentyfour-child/single.php
@@ -1,33 +1,10 @@
-<?php get_header(); ?>
-
-<article class="single-post">
-    <h1><?php the_title(); ?></h1>
-    <div class="post-meta">
-        <span>By <?php the_author(); ?></span> | 
-        <span><?php the_date(); ?></span>
-    </div>
-    <div class="featured-image">
-        <?php the_post_thumbnail('large'); ?>
-    </div>
-    <div class="post-content">
-        <?php the_content(); ?>
-    </div>
-    <div class="related-posts">
-        <h3>Related Posts</h3>
-        <?php
-        $related = new WP_Query([
-            'posts_per_page' => 3,
-            'category__in'   => wp_get_post_categories(get_the_ID()),
-            'post__not_in'   => [get_the_ID()]
-        ]);
-        if ($related->have_posts()) :
-            while ($related->have_posts()) : $related->the_post(); ?>
-                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            <?php endwhile;
-            wp_reset_postdata();
-        endif;
-        ?>
-    </div>
-</article>
-
+<?php
+get_header(); ?>
+<div class="custom-single-post">
+    <?php while (have_posts()) : the_post(); ?>
+        <h1><?php the_title(); ?></h1>
+        <p>By <?php the_author(); ?> on <?php the_date(); ?></p>
+        <div><?php the_content(); ?></div>
+    <?php endwhile; ?>
+</div>
 <?php get_footer(); ?>
